989c912141812d3dfc6e651985eb404526f18f6d
Samiruzjaman Ashiq <ashiq88904@gmail.com>
2025-03-21 21:32:25 +0600

    Create Code-6(area of circle).c

---
+++ Code-6(area of circle).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define PI 3.14159
+
+int main() {
+    float radius, area;
+
+    // Input the radius
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+  
+    printf("The area of the circle is: %.2f\n", area);
+
+    return 0;
+}
